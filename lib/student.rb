b1d8bd4675d0fe19bc83537910bda23179d2cb02
Gregory Losco <gtlosco@gmail.com>
2020-01-09 02:01:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,16 +13,18 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student_hash|
-      
+      Student.new(student_hash)
     end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attr, value|
+      self.send('#{attr}=', value)
+    end 
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
