7781b1c7071ffbeafee9ddc1eee530c9cd3f2068
Gregory Losco <gtlosco@gmail.com>
2020-01-09 02:02:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -38,6 +38,6 @@ class Scraper
   student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
   
     student
-
+  end
 end
 
